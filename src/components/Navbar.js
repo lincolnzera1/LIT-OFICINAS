7f473a12806ccfa4a31482a3aed39f3429587a93
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-08-13 13:36:40 -0300

    Another changes to test heroku

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <div>
-        <h1>Bem vindo ao começo do site</h1>
+        <header>
+            <h1>Bem vindo ao começo do site</h1>
+        </header>
     </div>
   )
 }
