16ca69671181cf9b00e06b190204333d0a3d69f1
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-08-13 14:09:59 -0300

    Making the header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,13 @@ const Navbar = () => {
     <div>
         <header>
             <h1>Bem vindo ao começo do site</h1>
+            <menu>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Equipes</li>
+                    <li>Votações</li>
+                </ul>
+            </menu>
         </header>
     </div>
   )
